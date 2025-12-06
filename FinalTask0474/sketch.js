2d8ddf51bc446f481759d992faa6fd090f39fd6f
yntteogii <wldbswldbs03@naver.com>
2025-12-07 00:15:12 +0900

    1
    - 재생/정지버튼
    - 도형

---
+++ FinalTask0474/sketch.js
@@ -0,0 +1,51 @@
+let song;
+let fft;
+let playing = false;
+
+let playButtonX = 20, playButtonY = 20, btnW = 80, btnH = 40;
+
+function preload() {
+  song = loadSound('assets/alarmmusic.mp3');
+}
+
+function setup() {
+  createCanvas(800, 600);
+  fft = new p5.FFT();
+}
+
+function draw() {
+  background(20);
+
+  fill(playing ? "#ff5959" : "#59ff59");
+  rect(playButtonX, playButtonY, btnW, btnH, 10);
+  fill(0);
+  textAlign(CENTER, CENTER);
+  text(playing ? "STOP" : "PLAY", playButtonX + btnW/2, playButtonY + btnH/2);
+
+  if (playing) {
+    let bass = fft.getEnergy("bass");
+    let treble = fft.getEnergy("treble");
+    
+    let bassSize = map(bass, 0, 255, 50, 200);
+    let trebleSize = map(treble, 0, 255, 20, 120);
+
+    ellipse(width/2, height/2, bassSize);
+    rectMode(CENTER);
+    rect(width/2, height/2, trebleSize, trebleSize / 2);
+  }
+}
+
+function mousePressed() {
+  if (
+    mouseX > playButtonX && mouseX < playButtonX + btnW &&
+    mouseY > playButtonY && mouseY < playButtonY + btnH
+  ) {
+    if (!playing) {
+      song.play();
+      playing = true;
+    } else {
+      song.stop();
+      playing = false;
+    }
+  }
+}
