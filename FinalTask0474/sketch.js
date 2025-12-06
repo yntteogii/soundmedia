fec56ebc52709a2d70485c72e3f78980533dac02
yntteogii <wldbswldbs03@naver.com>
2025-12-07 00:21:22 +0900

    변화 좀 더 크게

--- FinalTask0474/sketch.js
+++ FinalTask0474/sketch.js
@@ -2,6 +2,7 @@ let song;
 let fft;
 let playing = false;
 
+let colorModeType = 0;
 let playButtonX = 20, playButtonY = 20, btnW = 80, btnH = 40;
 
 function preload() {
@@ -20,21 +21,38 @@ function draw() {
   rect(playButtonX, playButtonY, btnW, btnH, 10);
   fill(0);
   textAlign(CENTER, CENTER);
-  text(playing ? "STOP" : "PLAY", playButtonX + btnW/2, 
-playButtonY + btnH/2);
+  text(playing ? "STOP" : "PLAY", playButtonX + btnW/2, playButtonY + btnH/2);
 
   if (playing) {
     let bass = fft.getEnergy("bass");
     let treble = fft.getEnergy("treble");
 
-    let bassSize = map(bass, 0, 255, 50, 200);
-    let trebleSize = map(treble, 0, 255, 20, 120);
-    let shake = map(bass, 0, 255, -30, 30);
+    let r, g, b;
+    if (colorModeType === 0) {
+      r = map(bass, 0, 255, 80, 255);
+      g = map(treble, 0, 255, 50, 200);
+      b = map(treble, 0, 255, 120, 255);
+    } else {
+      let hue = frameCount % 360;
+      colorMode(HSB);
+      let col = color(hue, 255, 255);
+      colorMode(RGB);
+      r = red(col);
+      g = green(col);
+      b = blue(col);
+    }
 
+    let bassSize = map(bass, 0, 255, 50, 300);
+    let trebleSize = map(treble, 0, 255, 20, 200);
+    let shake = map(bass, 0, 255, -60, 60);
+
+    fill(r, g, b);
     ellipse(width/2, height/2, bassSize);
     rectMode(CENTER);
-    rect(width/2, height/2, trebleSize, trebleSize / 2);
+    rect(width/2, height/2, trebleSize * 1.2, trebleSize / 1.2);
 
+    stroke(r, g, b);
+    strokeWeight(4);
     line(0, height/2 + shake, width, height/2 - shake);
   }
 }
@@ -51,5 +69,11 @@ function mousePressed() {
       song.stop();
       playing = false;
     }
+  }
+}
+
+function keyPressed() {
+  if (key === 'c' || key === 'C') {
+    colorModeType = (colorModeType + 1) % 2;
   }
 }
