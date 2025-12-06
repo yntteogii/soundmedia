b16422fdba858c500b601636c512da05235846ec
yntteogii <wldbswldbs03@naver.com>
2025-12-07 00:42:16 +0900

    위치 조정

--- FinalTask0474/sketch.js
+++ FinalTask0474/sketch.js
@@ -1,53 +1,66 @@
-let song;
-let fft;
-let playing = false;
+let song;               
+let fft;                
+let playing = false;    
 
-let colorModeType = 0;
+let colorModeType = 0;  
 let playButtonX = 20, playButtonY = 20, btnW = 80, btnH = 40;
 
 function preload() {
-  song = loadSound('assets/alarmmusic.mp3');
+  song = loadSound('assets/alarmmusic.mp3');  
 }
 
 function setup() {
   createCanvas(800, 600);
-  fft = new p5.FFT();
+  fft = new p5.FFT();  
 }
 
 function draw() {
   background(15);
-  drawPlayStopButton();
+
+  drawPlayStopButton(); 
 
   if (playing) {
-    let spectrum = fft.analyze();
-    let bass = fft.getEnergy("bass");      
-    let treble = fft.getEnergy("treble");  
-    let bassSize = map(bass, 0, 255, 50, 300);
-    let trebleSize = map(treble, 0, 255, 20, 200);
-    let shake = map(bass, 0, 255, -60, 60);
-    let c = chooseColor(bass, treble);
+    let spectrum = fft.analyze(); 
+    let bass = fft.getEnergy("bass");     
+    let treble = fft.getEnergy("treble"); 
 
-    fill(c.r, c.g, c.b);
+  
+    let bassSize = map(bass, 0, 255, 50, 350);
+    let trebleSize = map(treble, 0, 255, 20, 250);
+    let shake = map(bass, 0, 255, -80, 80);
+
+    let c = chooseColor(bass, treble); 
+
+  
+    fill(c.r, c.g, c.b, 190);
+    noStroke();
     ellipse(width/2, height/2, bassSize);
+
+    
     push();
     translate(width/2, height/2);
-    rotate(frameCount * 0.01);
+    rotate(frameCount * 0.015);
+    fill(c.r, c.g, c.b, 130);
     rectMode(CENTER);
-    rect(0, 0, trebleSize * 1.2, trebleSize / 1.2);
+    rect(0, 0, trebleSize * 1.3, trebleSize / 1.4);
     pop();
 
-    stroke(c.r, c.g, c.b);
+
+    stroke(c.r, c.g, c.b, 220);
+    strokeWeight(4);
     line(0, height/2 + shake, width, height/2 - shake);
   }
 }
 
 function chooseColor(bass, treble) {
   let r, g, b;
+
   if (colorModeType === 0) {
     r = map(bass, 0, 255, 80, 255);
     g = map(treble, 0, 255, 50, 200);
     b = map(treble, 0, 255, 120, 255);
-  } else {
+
+  } else if (colorModeType === 1) {
     let hue = frameCount % 360;
     colorMode(HSB);
     let col = color(hue, 255, 255);
@@ -55,17 +68,28 @@ function chooseColor(bass, treble) {
     r = red(col);
     g = green(col);
     b = blue(col);
+
+  } else {
+    let base = map(bass + treble, 0, 510, 255, 0);
+    r = base;
+    g = 255 - base;
+    b = (bass + treble) % 255;
   }
+
   return { r, g, b };
 }
+
 
 function drawPlayStopButton() {
   fill(playing ? "#ff5959" : "#59ff59");
   rect(playButtonX, playButtonY, btnW, btnH, 10);
+
   fill(0);
+  textSize(18);
   textAlign(CENTER, CENTER);
   text(playing ? "STOP" : "PLAY", playButtonX + btnW/2, playButtonY + btnH/2);
 }
+
 
 function mousePressed() {
   if (
@@ -82,8 +106,9 @@ function mousePressed() {
   }
 }
 
+
 function keyPressed() {
   if (key === 'c' || key === 'C') {
-    colorModeType = (colorModeType + 1) % 2;
+    colorModeType = (colorModeType + 1) % 3;
   }
 }
